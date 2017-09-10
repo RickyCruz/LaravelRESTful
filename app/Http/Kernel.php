3a69fe1b2a4f67c44d213ff627c370910543ccc1
Ricky Cruz <rczdev@gmail.com>
2017-09-09 23:10:00 -0500

    Register middlewares to check Scopes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,6 +60,8 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         // 'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'throttle' => \App\Http\Middleware\CustomThrottleRequests::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
         'signature' => \App\Http\Middleware\SignatureMiddleware::class,
         'transform.input' => \App\Http\Middleware\TransformInput::class,
     ];
