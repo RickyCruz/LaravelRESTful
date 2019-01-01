67d472837585bb12b5029af43ee6df50e1e1aca7
Ricky Cruz <rczdev@gmail.com>
2019-01-01 13:51:00 -0600

    Allow CORS in Error Responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use App\Traits\ApiResponse;
+use Barryvdh\Cors\CorsService;
 use Illuminate\Database\QueryException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Session\TokenMismatchException;
@@ -54,6 +55,15 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
+    {
+        $response = $this->handleException($request, $exception);
+
+        app(CorsService::class)->addActualRequestHeaders($response, $request);
+
+        return $response;
+    }
+
+    public function handleException($request, Exception $exception)
     {
         if ($exception instanceof ValidationException) {
             $errors = $exception->validator->errors()->getMessages();
